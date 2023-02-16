6129def7b42cfad7845f1f8f3999c5240ba1aa4a
Jakub Padło <jakubpadlo11@gmail.com>
2023-02-16 15:05:34 +0100

    Add config schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { configValidationSchema } from './config.schema';
 @Module({
   imports: [
     PrismaModule,
     ConfigModule.forRoot({
       envFilePath: [`.env.stage.${process.env.STAGE}`],
+      validationSchema: configValidationSchema,
     }),
   ],
 })
